bf0a4b09d384b6bb011a8784b2c8822f6d3c3a03
rombdn99 <55712933+rombdn99@users.noreply.github.com>
2019-11-08 07:45:50 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<?php
+echo "<footer style='position: absolute; bottom: 0px; width: 99%; text-align:center;'>";
+echo "<a  href='mailto:raul41999@gmail.com' >Site developed by: Raul Osuna.</a>";
+echo "</footer>";
+?>
